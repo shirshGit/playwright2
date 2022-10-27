8c6f7e6f3f02ea611c2dfb237a343094617892ab
Prachi Chourasia <pchourasia@catchpoint.com>
2022-10-27 12:14:49 +0530

    updated resources file.

--- resources/stagingdata.ts
+++ resources/stagingdata.ts
@@ -1,16 +1,22 @@
 export const stagedata = {
-    'node' : 'Boston - Verizon - ST', //for creating test.
     'alertRecipientName' : 'Client Admin DoNotDelete', //for creating items which needed contact.
+    'automationContact' : 'userrole9_DoNOTDeleteqaautomationxclient-clientadministrator@catchpoint.com', //contact used for automation, where assiging other userrole is required
+	
+    'defaultUserrole' : 'Client Administrator',//for contact default system access
+
+    'node' : 'Boston - Verizon - ST', //for creating test.
+    
     'productForJunkItems' : 'Selenium', //for creating folder and test for automation.
-    'url' : 'https://www.google.com', //for creating items e.g synthetic test,endpoint test.
+    'productForCopyTest' : 'ProductForCopyScenariosDoNotDelete',//product for copy test scenarios
+    'password' : 'Password123!',
+
     'testID' : '240905', // used for automation scripts where test are not being created.
     'testListPageUrl' : 'https://portalstage.catchpoint.com/ui/Symphony/ControlCenter/Tests', //for validation of synthetic cc page.
-	 'productForCopyTest' : 'ProductForCopyScenariosDoNotDelete',//product for copy test scenarios
-    'userroleName1' : 'OnlyManageTestPermission',//user role with only test and test templates permission
-	'userroleName2' : 'OnlyManageRumAndTestTemplatePermission',// userrole with only rum and test templates permission
-	'userroleName3' : 'DivisionLevelAccess',//used for division level access
-	'userroleName4' : 'DoNotHaveAccessForDivision',//used for dont have access for division
-	'userrole9' : 'userrole9_DoNOTDeleteqaautomationxclient-clientadministrator@catchpoint.com', //contact used for automation, where assiging other userrole is required
-	'password' : 'Password123!',
-	'defaultUserrole' : 'Client Administrator'//for contact default system access
+	
+    'userRoleManageTests' : 'OnlyManageTestPermission',//user role with only test and test templates permission
+	'userRoleManageRumAndTestTemplate' : 'OnlyManageRumAndTestTemplatePermission',// userrole with only rum and test templates permission
+	'userRoleManageDivisionLevel' : 'DivisionLevelAccess',//used for division level access
+	'userRoleDoNotHavePermissionForDivision' : 'DoNotHaveAccessForDivision',//used for dont have access for division
+	'url' : 'https://www.google.com', //for creating items e.g synthetic test,endpoint test.
+    
 }
