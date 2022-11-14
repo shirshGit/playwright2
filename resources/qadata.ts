d80b714ffcf2ff5378895fd4693d71d83ecefcdf
Prachi Chourasia <pchourasia@catchpoint.com>
2022-11-14 10:23:36 +0530

    updated QA and Stage resource file.

--- resources/qadata.ts
+++ resources/qadata.ts
@@ -4,21 +4,36 @@ export const qadata = {
 	
     'defaultUserrole' : 'Client Administrator',//for contact default system access
     
+    'imapTestID' : '276990', // used for records automation
+
+    'mqttTestID' : '276989',// used for records automation
+
     'node' : 'Denver - Level3 - QA',   //for creating test
+    'ntpTestID' : '276988',// used for records automation
     
     'productForJunkItems' : 'Selenium',  //for creating folder and test for automation
     'productForCopyTest' : 'ProductForCopyScenariosDoNotDelete',//product for copy test scenarios
     'password' : 'Password123!',
+
+    'rumAppName' : 'Automation_WebApp_DoNotEdits',//used for automation
     
     'testID' : '264592',// used for automation scripts where test are not being created.
     'testListPageUrl' : 'https://qaportal.catchpoint.com/ui/Symphony/ControlCenter/Tests', //for validation of synthetic cc page
+    'transactionTest' : '211911', // used for records automation
+    'transactionTestIDWithEnforceFailureAdvanceSetting' : '269328',//used for records automation
+    'transactionTestWithMultipleStepAndStepName' : '272641', // used for records automation
+    'transactionTestWithMultipleStepWithoutStepName' : '272642',//used for records automation
     
     'userRoleManageTests' : 'OnlyManageTestPermission',//user role with only test and test templates permission
 	'userRoleManageRumAndTestTemplate' : 'OnlyManageRumAndTestTemplatePermission',// userrole with only rum and test templates permission
 	'userRoleManageDivisionLevel' : 'DivisionLevelAccess',//used for division level access
 	'userroleDoNotHavePermissionForDivision' : 'DoNotHaveAccessForDivision',//used for do not have access for division
 	'url' : 'https://www.google.com', //for creating items e.g synthetic test,endpoint test
+    
+    'webPingTestID' : '211914',// used for records automation
+    'webPingTestIDForDivision' : '268968',// used for records automation
 }
+    
     
     
     
