05219fe3fbb78f7ab3dad44052f448c88a098f7b
sgiri <sgiri@catchpoint.com>
2023-02-13 09:48:42 +0530

    adding TestUnit to Junit parser.

---
+++ TestLinkReportParser/TestLinkCompatibleJsonToJunitReport/Program.cs
@@ -0,0 +1,18 @@
+﻿using JsonReportToTestLinkCompatibleReport;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestLinkCompatibleJsonToJunitReport
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            JsonReportToXmlReport.CreateXmlFromJson(JsonReportToXmlReport.ReadJsonReport());
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
